7336243757e7a3ba51cf28bcaed52a90416ae9d2
rparitosh <rparitosh@gmail.com>
2022-09-08 11:49:02 -0500

    Initial commit

---
+++ src/main/java/com/paritosh/eshop/productservice/EshopProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paritosh.eshop.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paritosh/eshop/productservice/EshopProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paritosh.eshop.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
